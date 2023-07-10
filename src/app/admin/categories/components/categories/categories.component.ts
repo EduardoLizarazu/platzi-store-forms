0c3b0783194ac6d287626a77e39deb0b8d103f3f
eduardolizarazu <jelo2021@gmail.com>
2023-07-10 11:48:37 -0400

    Get All categories and use Angular Mat

--- src/app/admin/categories/components/categories/categories.component.ts
+++ src/app/admin/categories/components/categories/categories.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CategoriesService } from "../../../../core/services/categories.service";
+import { Observable } from "rxjs";
+import { Category } from "./../../../../core/models/category.model";
+
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  public categories$: Observable<Category[]>;
+
+  constructor(
+    private categoriesService: CategoriesService
+  ) { }
 
   ngOnInit(): void {
+    this.getAllInfo();
+  }
+  private getAllInfo() {
+    this.categories$ = this.categoriesService.getAllCategories();
   }
 
 }
