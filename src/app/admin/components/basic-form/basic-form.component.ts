b36747e52505a467f04a33de8eb86dbfdc2e96d1
eduardolizarazu <jelo2021@gmail.com>
2023-07-09 22:53:33 -0400

    Usando componentes de Angular Material 2

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -7,16 +7,13 @@ import { FormControl, Validators, FormGroup, FormBuilder } from "@angular/forms"
 @Component({
   selector: 'app-basic-form',
   templateUrl: './basic-form.component.html',
-  styleUrls: ['./basic-form.component.scss']
+  styleUrls: ['./basic-form.component.scss'],
 })
 export class BasicFormComponent implements OnInit {
-
   // Tipamos el form
   form: FormGroup;
 
-  constructor(
-    private formBuilder: FormBuilder
-  ) {
+  constructor(private formBuilder: FormBuilder) {
     // Inicializar el form
     this.buildForm();
   }
@@ -35,17 +32,13 @@ export class BasicFormComponent implements OnInit {
 
   getNameValue() {
     // Capturar el valor del form control en el instante
-    console.log(this.nameField.value);
+    console.log(this.name().value);
   }
 
   // Submit del formulario y validacion
-  save(event: Event){
-    if(this.form.valid){
-      console.log(this.form.value);
-    }else{
-      // Marcar todos los campos como tocados (ver errores)
-      this.form.markAllAsTouched();
-    }
+  save(event: Event) {
+    this.form.valid ?
+      console.log(this.form.value) : this.form.markAllAsTouched();
   }
 
   private buildForm() {
@@ -53,67 +46,88 @@ export class BasicFormComponent implements OnInit {
     // Manejando multiples forms groups
     this.form = this.formBuilder.group({
       fullName: this.formBuilder.group({
-        name: ['', [Validators.required, Validators.maxLength(10), Validators.pattern(/^[a-zA-Z ]+$/)]],
-        last: ['', [Validators.required, Validators.maxLength(10), Validators.pattern(/^[a-zA-Z ]+$/)]]
+        name: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(10),
+            Validators.pattern(/^[a-zA-Z ]+$/),
+          ],
+        ],
+        last: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(10),
+            Validators.pattern(/^[a-zA-Z ]+$/),
+          ],
+        ],
       }),
       email: ['', [Validators.required, Validators.email]],
       phone: ['', Validators.required],
-      color: ['#000000'],
-      date: [''],
-      age: [18, [Validators.required, Validators.min(18), Validators.max(100)]],
-      category: [''],
-      tag: [''],
+      color: ['#FFFFFF', Validators.required],
+      date: ['', Validators.required],
+      age: ['', [Validators.required, Validators.min(18), Validators.max(100)]],
+      url: ['', Validators.required],
+      category: ['', Validators.required],
+      tag: ['', Validators.required],
       agree: [false, [Validators.requiredTrue]],
-      gender: [''],
-      zone: [''],
+      gender: ['', Validators.required],
+      zone: ['', Validators.required],
     });
   }
 
   // Obtener el validador de un form group
-  get nameField() {
-    // return this.form.get('fullName').get('name'); // same as below
-    return this.form.get('fullName.name');
-  }
-  get lastField() {
-    return this.form.get('fullName.last');
-  }
-  get emailField() {
-    return this.form.get('email');
-  }
-  get phoneField() {
-    return this.form.get('phone');
-  }
-  get colorField() {
-    return this.form.get('color');
-  }
-  get dateField() {
-    return this.form.get('date');
-  }
-  get ageField() {
-    return this.form.get('age');
-  }
-  get categoryField() {
-    return this.form.get('category');
-  }
-  get tagField() {
-    return this.form.get('tag');
-  }
-  get agreeField() {
-    return this.form.get('agree');
-  }
-  get genderField() {
-    return this.form.get('gender');
-  }
-  get zoneField() {
-    return this.form.get('zone');
-  }
+  name = () => this.form.get('fullName.name');
+  last = () => this.form.get('fullName.last');
+  email = () => this.form.get('email');
+  phone = () => this.form.get('phone');
+  color = () => this.form.get('color');
+  date = () => this.form.get('date');
+  age = () => this.form.get('age');
+  url = () => this.form.get('url');
+  category = () => this.form.get('category');
+  tag = () => this.form.get('tag');
+  agree = () => this.form.get('agree');
+  gender = () => this.form.get('gender');
+  zone = () => this.form.get('zone');
 
-  // Para evitar repetir codigo de validacion en el html
-  get isNameFieldValid() {
-    return this.nameField.touched && this.nameField.valid;
-  }
-  get isNameFieldInvalid() {
-    return this.nameField.touched && this.nameField.invalid;
-  }
+  isNameValid = () => this.name().valid && this.name().touched;
+  isNameInvalid = () => this.name().invalid && this.name().touched;
 
+  isLastValid = () => this.last().valid && this.last().touched;
+  isLastInvalid = () => this.last().invalid && this.last().touched;
+
+  isEmailValid = () => this.email().valid && this.email().touched;
+  isEmailInvalid = () => this.email().invalid && this.email().touched;
+
+  isPhoneValid = () => this.phone().valid && this.phone().touched;
+  isPhoneInvalid = () => this.phone().invalid && this.phone().touched;
+
+  isColorValid = () => this.color().valid && this.color().touched;
+  isColorInvalid = () => this.color().invalid && this.color().touched;
+
+  isDateValid = () => this.date().valid && this.date().touched;
+  isDateInvalid = () => this.date().invalid && this.date().touched;
+
+  isAgeValid = () => this.age().valid && this.age().touched;
+  isAgeInvalid = () => this.age().invalid && this.age().touched;
+
+  isUrlValid = () => this.url().valid && this.url().touched;
+  isUrlInvalid = () => this.url().invalid && this.url().touched;
+
+  isCategoryValid = () => this.category().valid && this.category().touched;
+  isCategoryInvalid = () => this.category().invalid && this.category().touched;
+
+  isTagValid = () => this.tag().valid && this.tag().touched;
+  isTagInvalid = () => this.tag().invalid && this.tag().touched;
+
+  isAgreeValid = () => this.agree().valid && this.agree().touched;
+  isAgreeInvalid = () => this.agree().invalid && this.agree().touched;
+
+  isGenderValid = () => this.gender().valid && this.gender().touched;
+  isGenderInvalid = () => this.gender().invalid && this.gender().touched;
+
+  isZoneValid = () => this.zone().valid && this.zone().touched;
+  isZoneInvalid = () => this.zone().invalid && this.zone().touched;
 }
